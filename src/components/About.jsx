5d86e70cda0f44f3c7919548ff34b23e11edecef
bijay28-dev <vk57713@gmail.com>
2024-06-05 18:53:02 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { AiOutlineCloudDownload } from 'react-icons/ai'
+import '../App.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { clamp } from 'framer-motion'
+
+const About = () => {
+    useEffect(() => {
+        AOS.init({ duration: 1000 })
+    })
+
+    return (
+        <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
+                <div className='pb-8'>
+
+                    <h2 className='text-4xl  font-bold inline border-b-4 border-gray-500'>About</h2>
+                </div>
+                <p data-aos="slide-up" data-aos-duration="500" className='text-gray-400 mt-5 ease-in fade-in'>
+                    I completed my B.Tech in Electrical Engineering from Gandhi Institute for Technology in 2021. Along the way, I explored various fields, including Web App Development, Content Creation, Freelancing, and Entrepreneurship. Devloped various web applications usting the technologies such as Java, React and NodeJS. Having expertise in Java for the back-end development, built and delivered various end-to-end projects to my clients.
+                </p>
+                <p data-aos="slide-up" data-aos-duration="500" className='text-gray-400 mt-5 ease-in fade-in'>
+                    I'm continuously working with many projects for my clients as well as my personal projects. I have an Instagram page (@bijay.codes) where I create content for those who are just starting with programming.
+                </p>
+                <div data-aos="zoom-in" data-aos-duration="500" className='mb-20 fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'>
+
+
+                    <AiOutlineCloudDownload />
+                    <a href="/Bijay_Kumar_Tudu_Resume.pdf" download>Download CV</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Experience.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react'
+import html from '../assets/experience/html.png'
+import css from '../assets/experience/css.png'
+import javascript from '../assets/experience/javascript.png'
+import mongodb from '../assets/experience/mongodb.png'
+import node from '../assets/experience/node.png'
+import react from '../assets/experience/react.png'
+import java from '../assets/experience/java.png'
+import mysql from '../assets/experience/mysql.png'
+import github from '../assets/experience/github.png'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+const Experience = () => {
+    useEffect(() => {
+        AOS.init({ duration: 1000 })
+    })
+
+    const techs = () => [
+        {
+            id: 1,
+            scr: java,
+            title: 'Java',
+            style: 'shadow-red-800'
+        },
+        {
+            id: 2,
+            scr: mysql,
+            title: 'MySQL',
+            style: 'shadow-blue-400'
+        },
+        {
+            id: 3,
+            scr: github,
+            title: 'Github',
+            style: 'shadow-blue-50'
+        },
+        {
+            id: 4,
+            scr: html,
+            title: 'HTML',
+            style: 'shadow-orange-500'
+        },
+        {
+            id: 5,
+            scr: css,
+            title: 'CSS',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 6,
+            scr: javascript,
+            title: 'JAVASCRIPT',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 7,
+            scr: mongodb,
+            title: 'MongoDB',
+            style: 'shadow-green-600'
+        },
+        {
+            id: 8,
+            scr: react,
+            title: 'React',
+            style: 'shadow-blue-400'
+        },
+        {
+            id: 9,
+            scr: node,
+            title: 'Node JS',
+            style: 'shadow-green-400'
+        }
+    ]
+
+    return (
+        <div name="skills" className='bg-gradient-to-b from-gray-800 to-black text-white
+     w-full h-full'>
+            <div className='max-w-screen-lg max-h-screen-lg p-4 mx-auto flex flex-col justify-center w-full '>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-400'>Skills</p>
+                    <p className='py-6'>These are the Programming Languages, Framework and Libraries I've worked with</p>
+                </div>
+
+                <div className='w-full h-full grid grid-cols-2 text-center sm:grid-cols-3 gap-8 px-12 sm:px-0'>
+                    {
+                        techs().map(({ id, scr, title, style }) => (
+
+                            <div data-aos="zoom-in" data-aos-duration="500" key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                                <img src={scr} alt="" className='w-20 mx-auto' />
+                                <p className='mt-4'>{title}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience
